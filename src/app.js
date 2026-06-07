153adf564e2537286b902d3dd2136e6fd8daea90
Sanjay Kumar Parida <kumarparidasanjay23@gmail.com>
2026-06-07 20:50:58 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get('/', async (req, res) => {
   const { shortCode } = req.params;
 
   try {
-    res.send({"message":"Fuck you bitch anish"});
+    res.send({"message":"Fuck you bitch!"});
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: 'internal server error' });
